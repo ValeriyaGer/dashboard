fbb21d824f52e036dc263367825b7bee96865789
ValeriyaGer <43153180+ValeriyaGer@users.noreply.github.com>
2019-03-09 02:08:15 +0300

    Add files via upload

---
+++ precache-manifest.001aa9d4e42d0f4cebebb8d71c099c82.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fa4cf97a89d4c21ecfec",
+    "url": "/dashboard/static/js/runtime~main.fa4cf97a.js"
+  },
+  {
+    "revision": "707c467ab12bfa8e763f",
+    "url": "/dashboard/static/js/main.707c467a.chunk.js"
+  },
+  {
+    "revision": "6819fdd1fcb131259bc9",
+    "url": "/dashboard/static/js/2.6819fdd1.chunk.js"
+  },
+  {
+    "revision": "707c467ab12bfa8e763f",
+    "url": "/dashboard/static/css/main.a67b35d1.chunk.css"
+  },
+  {
+    "revision": "6819fdd1fcb131259bc9",
+    "url": "/dashboard/static/css/2.9d7b86ac.chunk.css"
+  },
+  {
+    "revision": "7c829a3a5a0b35a7c3bf8063f6601ee3",
+    "url": "/dashboard/index.html"
+  }
+];
